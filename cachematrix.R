713c6a9b653161b09e145eef43138c60af2ede2e
rakeshpadma20 <rockypads@outlook.com>
2020-04-25 16:59:41 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Put comments here that give an overall description of what your	
+## Our aim in this experiment is to write a pair of functions, namely, 
+## functions do	
+## "makeCacheMatrix" and "cacheSolve" that cache the inverse of a matrix
+## Write a short comment describing this function	
+## makeCacheMatrix is a function which creates a special "matrix" object that can 
+## cache its inverse for the input (which is an invertible square matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-	inverse <- NULL
+	inv <- NULL
 	set <- function(y) {
 		x <<- y
-		m <<- NULL
+		inv <<- NULL
 	}
 	get <- function() x
-	setinverse <- function(i) inverse <- i
-	getinverse <- function() inverse
-	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+	setinv <- function(inverse) inv <<-inverse
+	getinv <- function() inv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
 ## Write a short comment describing this function
+## cacheSolve is a function which computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inverse <- x$getinverse()
-        if(!is.null(inverse)) {
+        inv <- x$getinv()
+        if(!is.null(inv)) {
         	message("getting cached inverse")
-        	return(inverse)
+        	return(inv)
         }
         data <- x$get()
-        inverse <- solve(data, ...)
-        x$setinverse(inverse)
-        inverse
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
 
-# data <- matrix(c(1, 1, 4, 0, 3, 1, 4, 4, 0), nrow=3, ncol=3)
+## ---------------Checking the program------------------------
+## m <- matrix(rnorm(16),4,4)
+## m1 <- makeCacheMatrix(m)
+## cacheSolve(m1)
+
+## [,1]       [,2]       [,3]       [,4]
+## [1,] -0.1653269  0.2592203  0.6176218 -0.7520955
+## [2,]  0.2828334 -0.1853499  0.4511382  0.2094365
+## [3,]  0.1434840  1.0413868 -0.3550853 -0.3261154
+## [4,]  0.1793583 -0.4252171 -0.4371493 -0.1749830
